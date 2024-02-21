ee281a7d73d4ca260efbac6424dee8a7505a32ee
jeff.sylvester <jeff.sylvester@prizepicks.com>
2024-02-21 11:22:43 -0500

    add main action structure

---
+++ index.js
@@ -0,0 +1,61 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function main(){
+    try {
+        const commits = await getCommits()
+        const ids = extractIssueIds(commits)
+        console.log(ids)
+    } catch (error) {
+        core.setFailed(error.message);
+    }
+}
+
+async function getCommits() {
+    // const owner = core.getInput('owner', { required: true });
+    // const repo = core.getInput('repo', { required: true });
+    // const pr_number = core.getInput('pr_number', { required: true });
+    // const token = core.getInput('token', { required: true });
+    const owner = 'slyjeff';
+    const repo = 'repotest';
+    const pr_number = 3;
+    const token = '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';
+
+    const octokit = new github.getOctokit(token);
+
+    return await octokit.request("GET /repos/{owner}/{repo}/pulls/{pr_number}/commits", {
+        owner: owner,
+        repo: repo,
+        pr_number: pr_number
+    });
+}
+
+function extractIssueIds(commits)  {
+    const issuePrefix = "issue-";
+
+    const ids = [];
+
+    commits.data.forEach(commit => {
+        const message = commit.commit.message
+        if (!message.startsWith("Merge pull request #")) {
+            return;
+        }
+
+        const issuePrefixIndex = message.indexOf(issuePrefix);
+        if (issuePrefixIndex === -1) {
+            return;
+        }
+
+        let id = message.substring(issuePrefixIndex + issuePrefix.length)
+        const newLineIndex = id.indexOf("\n")
+        if (newLineIndex > -1) {
+            id = id.substring(0, newLineIndex)
+        }
+
+        ids.push(id)
+    })
+
+    return ids
+}
+
+main();
